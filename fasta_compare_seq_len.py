51c345713c2ee3e677f708d447e753505d596974
etd530 <etd530@gmail.com>
2026-05-11 15:58:51 +0200

    - Renamed fasta_find_short_seqs.py to fasta_compare_seq_len.py - fasta_compare_seq_len.py: updated script to output a comparison of the query sequences' length to the mean, median and max of the reference sequences, without any a priori filtering

--- fasta_compare_seq_len.py
+++ fasta_compare_seq_len.py
@@ -4,13 +4,11 @@
 Given a reference FASTA and a queries FASTA, mark queries that are too short.
 
 Usage:
-    fasta_find_short_seqs.py <REFERENCE> <QUERIES> [--metric=STR] [--threshold=FLOAT] [--verbose] [--help]
+    fasta_compare_seq_len.py <REFERENCE> <QUERIES> [--verbose] [--help]
 
 Arguments:
     <REFERENCE>                  A FASTA file containing the reference sequences.
     <QUERIES>                    A FASTA file containing the query sequences.
-    --metric STR                 The metric to use for comparison. One of 'mean', 'median', 'max' [default: max].
-    --threshold FLOAT            The threshold for marking short sequences [default: 0.7].
     --verbose                    Print the progressions of the program to the terminal (Standard Error).
     --help                       Show this help message and exit.
 """
@@ -26,13 +24,7 @@ if __name__ == '__main__':
     args = docopt(__doc__)
     reference_file = args['<REFERENCE>']
     queries_file = args['<QUERIES>']
-    metric = args['--metric']
-    threshold = float(args['--threshold'])
     out_prefix = queries_file.split('.')[0].split('/')[-1]
-
-    if metric not in ['mean', 'median', 'max']:
-        print("Error: metric must be one of 'mean', 'median', 'max'.")
-        exit(1)
 
     #### MAIN ####
     # Get the length of all reference sequences
@@ -43,21 +35,20 @@ if __name__ == '__main__':
     print("Done reading reference file %s." % reference_file)
 
     # Compute the metric on the reference lengths
-    if metric == 'mean':
-        ref_metric = np.mean(len_list)
-    elif metric == 'median':
-        ref_metric = np.median(len_list)
-    elif metric == 'max':
-        ref_metric = np.max(len_list)
-    
+    ref_mean = np.mean(len_list)
+    ref_median = np.median(len_list)
+    ref_max = np.max(len_list)
+
     # Iterate over the query sequences and mark those that are too short
     print("Reading query file %s..." % queries_file)
-    out_string = ''
-    with open(out_prefix + '_short_seqs.txt', 'w') as out_short:
+    out_string = '1_seqid\t2_rel_len_mean\t3_rel_len_median\t4_rel_len_max\n'
+    with open(out_prefix + '.rel_len.tsv', 'w') as wh:
         for seq_record in SeqIO.parse(queries_file, "fasta"):
             seq_len = len(seq_record)
             seqid = seq_record.id
-            if seq_len < threshold * ref_metric:
-                out_string += '%s\n' % seqid
-        out_short.write(out_string)
+            rel_len_mean = seq_len / ref_mean
+            rel_len_median = seq_len / ref_median
+            rel_len_max = seq_len / ref_max
+            out_string += '%s\t%.2f\t%.2f\t%.2f\n' % (seqid, rel_len_mean, rel_len_median, rel_len_max)
+        wh.write(out_string)
     print("Done reading query file %s." % queries_file)
